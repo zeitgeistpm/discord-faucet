61b2ad26d0b21dff2612ffb53282f1fc9978fb19
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-02-18 23:09:25 +0530

    Update script to copy db and remove old build

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "Copying database to local..."
+docker cp main-faucet:main-faucet.db main-faucet.db
+
+echo "Removing main-faucet..."
+docker rm -f main-faucet
+
 echo "Building main-faucet..."
 docker build . --target main-faucet -t main-faucet
 
